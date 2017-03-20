a84c851bb87d3a6db8c9f50289182c16f78fe484
Dobri Kostadinov <dobri.kostadinov@gmail.com>
2017-03-21 00:26:34 +0200

    The spacewalk list is displayed in ascending order by date. When the button in the bottom right is clicked the list should be displayed in descending order. Clicking it again should reverse that - it should act as a toggle.

--- app/src/main/java/com/rockspin/androiddevtest/MainActivity.java
+++ app/src/main/java/com/rockspin/androiddevtest/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
+import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Call;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private EVActivityAdapter mAdapter;
+
+    private List<CosmonautData> cosmonautDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         MyApplication.getAPIService().getEVList().enqueue(new Callback<List<CosmonautData>>() {
             @Override
             public void onResponse(Call<List<CosmonautData>> call, Response<List<CosmonautData>> response) {
-                final List<CosmonautData> cosmonautDataList = response.body();
+                cosmonautDataList = response.body();
                 mAdapter.setCosmonautActivityList(cosmonautDataList);
             }
 
@@ -40,5 +44,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Error occur while loading data.", Toast.LENGTH_LONG).show();
             }
         });
+
+        findViewById(R.id.fab_change_sorting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reverseAdapter();
+            }
+        });
+
+    }
+
+    private void reverseAdapter() {
+        Collections.reverse(cosmonautDataList);
+
+        mAdapter.notifyDataSetChanged();
     }
 }
